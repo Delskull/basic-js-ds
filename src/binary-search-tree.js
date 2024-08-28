8a6513848b2ea4963064b61f4e5649b5961d2702
Sergey Stepanov <Deluta1989@gmail.com>
2024-08-28 16:08:53 +0300

    init: start,add root,add,has,find methods

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -6,26 +6,81 @@ const { NotImplementedError } = require('../extensions/index.js');
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+
+  }
+
+}
 class BinarySearchTree {
+  constructor() {
+    this.rootNode = null;
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.rootNode
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+   const newNode = new Node(data);
+   if (!this.rootNode){
+     return this.rootNode = newNode;
+   }
+    addNewNode(this.rootNode , newNode);
+   function  addNewNode (node, newNode){
+     if (newNode.data < node.data){
+       if (!node.left){
+         node.left = newNode;
+       }
+       else {
+         addNewNode(node.left , newNode)
+       }
+     }
+     else {
+       if (!node.right){
+         node.right = newNode
+       }
+       else {
+         addNewNode(node.right , newNode)
+       }
+     }
+   }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    return hasObj(this.rootNode, data);
+    function hasObj(node, data) {
+      if (!node) {
+        return false;
+      } else if (node.data === data) {
+        return true;
+      } else if (data < node.data) {
+        return hasObj(node.left, data);
+      } else {
+        return hasObj(node.right, data);
+      }
+    }
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+ return findObj(this.rootNode , data)
+    function findObj(node,data){
+   if (!node){
+     return null;
+   }
+   else if ( node.data === data){
+     return node;
+   }
+   else if ( node.data < data){
+     return findObj(node.right , data)
+   }
+   else if ( node.data > data){
+     return findObj(node.left , data)
+   }
+    }
   }
 
   remove(/* data */) {
